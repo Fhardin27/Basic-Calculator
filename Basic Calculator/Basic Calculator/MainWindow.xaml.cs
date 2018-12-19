fa6c95110885f583b7f644ad3a20ab1ea0a137e3
Fate Hardin <fhardin27@gmail.com>
2018-12-19 13:43:29 -0600

    More buttons added

--- Basic Calculator/Basic Calculator/MainWindow.xaml.cs
+++ Basic Calculator/Basic Calculator/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace Basic_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -71,6 +73,21 @@ namespace Basic_Calculator
         }
 
         private void Nine_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Times_Click(object sender, RoutedEventArgs e)
         {
 
         }
