5c82fcba8ec229e9008fc71311dc3000ad4851eb
Zach McLean <ohhodysseus@gmail.com>
2018-12-19 13:44:21 -0600

    more buttons

--- Basic Calculator/Basic Calculator/MainWindow.xaml.cs
+++ Basic Calculator/Basic Calculator/MainWindow.xaml.cs
@@ -32,45 +32,55 @@ namespace Basic_Calculator
 
         private void Zero_click(object sender, RoutedEventArgs e)
         {
-
+            number_box.Text += '0';
         }
 
         private void Two_click(object sender, RoutedEventArgs e)
         {
-
+            number_box.Text += '2';
         }
 
         private void Three_click(object sender, RoutedEventArgs e)
         {
-
+            number_box.Text += '3';
         }
 
         private void Four_click(object sender, RoutedEventArgs e)
         {
-
+            number_box.Text += '4';
         }
 
         private void Five_click(object sender, RoutedEventArgs e)
         {
-
+            number_box.Text += '5';
         }
 
         private void Six_click(object sender, RoutedEventArgs e)
         {
-
+            number_box.Text += '6';
         }
 
         private void Seven_click(object sender, RoutedEventArgs e)
         {
-
+            number_box.Text += '7';
         }
 
         private void Eight_click(object sender, RoutedEventArgs e)
         {
-
+            number_box.Text += '8';
         }
 
         private void Nine_click(object sender, RoutedEventArgs e)
+        {
+            number_box.Text += '9';
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
         {
 
         }
